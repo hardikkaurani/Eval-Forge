11024a21df8b79fb40d7578ee0fb91e5d2b81e94
Hardik Kaurani <hardikkaurani1@gmail.com>
2026-07-04 01:41:56 +0530

    feat(datasets): define custom dataset and benchmark exceptions

---
+++ backend/app/datasets/exceptions/__init__.py
@@ -0,0 +1,19 @@
+from app.datasets.exceptions.exceptions import (
+    BenchmarkSuiteNotFoundException,
+    DatasetException,
+    DatasetNotFoundException,
+    DatasetValidationException,
+    ExperimentNotFoundException,
+    InvalidDatasetFormatException,
+    StorageException,
+)
+
+__all__ = [
+    "DatasetException",
+    "DatasetNotFoundException",
+    "DatasetValidationException",
+    "InvalidDatasetFormatException",
+    "BenchmarkSuiteNotFoundException",
+    "ExperimentNotFoundException",
+    "StorageException",
+]
